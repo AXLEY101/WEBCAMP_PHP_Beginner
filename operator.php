c1bcea1b04b4d76581949b1ccd33c89291310b10
FujiTeru <FujiTeru@example.com>
2023-03-01 11:09:16 +0000

    operator.phpを書いた

---
+++ operator.php
@@ -0,0 +1,40 @@
+<?php
+
+$i = 1;
+echo "等しいかどうか \n";
+var_dump($i === 1);
+var_dump($i === 2);
+
+echo "\n";
+echo "等しくないかどうか \n";
+var_dump($i !== 1);
+var_dump($i !== 2);
+
+echo "\n";
+echo "等しいかどうか \n";
+$s = "abc";
+var_dump($s === "abc");
+var_dump($s === "ABC");
+var_dump($s === "a");
+
+// == と === の確認　その１
+echo "\n";
+echo "== と ===の確認その１ \n";
+var_dump($i == 1);
+var_dump($i === 1);
+
+// == と === の確認その２
+echo "\n";
+echo "== と ===の確認その２ \n";
+var_dump($i == "1");
+var_dump($i === "1");
+
+//2a問題の確認
+echo "\n";
+echo "2a問題の確認 \n";
+$i = 2;
+var_dump($i == "2a");
+var_dump($i === "2a");
+
+
+?>
