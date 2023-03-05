46f006a5e73ce7fed86603906cccb2b29f3b3aa6
FujiTeru <FujiTeru@example.com>
2023-03-05 06:43:30 +0000

    session.phpを書いた

---
+++ session.php
@@ -0,0 +1,23 @@
+<?php
+//バッファリング開始
+ob_start();
+//セッション開始
+session_start();
+
+function h(string $s):string
+{
+    return htmlspecialchars($s,ENT_QUOTES);
+}
+
+$counter = $_SESSION["counter"] ?? 1;
+
+echo "セッション）あなたがこのサイトを訪れたのは", h(strval($counter)),"回目ですね。";
+
+$next_counter = $counter + 1;
+
+$_SESSION["counter"] = $next_counter;
+
+ob_end_flush();
+
+
+?>
