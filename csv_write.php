0c3924e9301175a927c64997b1118f4cdc95e6e6
FujiTeru <FujiTeru@example.com>
2023-03-07 11:59:38 +0000

    csv_write.phpを書いた

--- csv_write.php
+++ csv_write.php
@@ -1,17 +1,23 @@
 <?php
 
-$write_date = [
+//書き込むべきデータの準備
+//「複数行」あるので、「配列の配列」でデータを持つ
 
+$write_data = [
         [1,2,3],
         ["aaa","bbb","ccc"],
-        [",aa","b\"b","c\n c"],
+        [",aa","b \" b","c \n c"],//カンマ区切りでダブルクォート、改行の入ったデータ
     ];
 
-    $file_name = __DIR__ . "/data.csv";
+//書き込むファイル名の把握
+$file_name = __DIR__ . "/data.csv";
+//ファイルのopen
+$file_obj = new SplFileObject($file_name,"w");
 
-    $file_obj = new SplFileObject($file_name, "w");
+//
+foreach($write_data as $datum){
+    //CSVの書き込み
+    $file_obj->fputcsv($datum);
+}
 
-    foreach($write_date as $datum){
-        $file_obj->fputcsv($datum);
-    }
 ?>
