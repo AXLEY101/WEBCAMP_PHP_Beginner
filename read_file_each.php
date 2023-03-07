4dafc6ad7e8ef3ad6d3560489ee5b30965ba3d5b
FujiTeru <FujiTeru@example.com>
2023-03-07 11:24:59 +0000

    read_file_each.php read_file_all.phpを書いた

--- read_file_each.php
+++ read_file_each.php
@@ -1,10 +1,10 @@
 <?php
-
+//ファイル名の把握
 $file_name = __FILE__;
-
+//ファイルのopen
 $file_obj = new SplFileObject($file_name);
-
+//各行を読みこんで出力する
 foreach($file_obj as $no => $line){
-    echo "{$no}:$line";
+    echo "{$no}: $line";
 }
 ?>
